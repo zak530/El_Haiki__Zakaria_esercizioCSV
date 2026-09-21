80c90c1eb2ad7cc349149e9c2224973c82d1a266
zakar <elhaiki.zakaria.studente@itispaleocapa.it>
2026-09-21 16:39:32 +0200

    aggiunta metodo aggiungistudente

---
+++ src/GestioneStudenti.java
@@ -0,0 +1,9 @@
+public class GestioneStudenti {
+    public void aggiungistudente(String nome, String cognome, String azienda, String vacanza) {
+
+        System.out.println(nome);
+        System.out.println(cognome);
+        System.out.println(azienda);
+        System.out.println(vacanza);
+    }
+}
